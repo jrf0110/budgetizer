e192c6ff1acd26bc6e56c9f051c78f4bd8dc9879
John Fawcett <jrf0110@gmail.com>
2017-06-13 19:27:29 -0500

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import sourcemaps from 'rollup-plugin-sourcemaps'
+import buble from 'rollup-plugin-buble'
 
 export default {
   entry: 'dist/client.js',
@@ -9,5 +10,8 @@ export default {
   plugins: [
     sourcemaps(),
     resolve({ jsnext: true, browser: true, }),
-  ]
+    buble({ exclude: ['src/'] }),
+  ],
+  // Disable warnings
+  // onwarn: (warning, next) => {},
 }
